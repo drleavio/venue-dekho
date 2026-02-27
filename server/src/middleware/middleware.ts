18ac555c8c13a3cf9f8ca1d1db0d7ae432be373d
Rahul Sharma <rahul.sharma@testaing.com>
2026-02-27 12:20:31 +0530

    added notification service

---
+++ server/src/middleware/middleware.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+
+export const middleware=async(req:any,res:any,next:any)=>{
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+    if (!token) {
+        req.user = null;
+        return res.status(203).json({success:false,message:"token not available"})
+    }
+    try {
+        const decoded=await jwt.verify(token,process.env.JWT_SECRET!) as any;
+        req.user=decoded;
+        next();
+    } catch (error) {
+        console.log("token error",error);
+        return res.json({success:false,message:"error getting token"})
+    }
+}
--- server/src/routes/protected/analysis.route.ts
+++ server/src/routes/protected/analysis.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { ClientMiddleware } from "../../middleware/Client.middleware.js";
 import { ClientAnalysis } from "../../controllers/client.analysis.js";
+import { middleware } from "../../middleware/middleware.js";
+import { createBooking } from "../../controllers/booking.controller.js";
 
 const router=Router();
 
 
 router.get("/analysis",ClientMiddleware,ClientAnalysis.getDashboard)
-router.post("/api/add",ClientMiddleware,ClientAnalysis.addVenue)
-router.put("/api/update-details",ClientMiddleware,ClientAnalysis.updateVenue)
+router.post("/add",ClientMiddleware,ClientAnalysis.addVenue)
+router.put("/update-details/:id",ClientMiddleware,ClientAnalysis.updateVenue)
+router.post("/book",middleware,createBooking)
 
 export default router
